d24a72dec596d4b038f1bf9dfde2b2267152b71b
wangyang12 <1075477190@qq.com>
2023-11-02 12:13:32 +1300

    commit2

---
+++ demo/src/main/java/com/example/demo/Add.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import Util.FileUtilE;
+import javafx.fxml.FXML;
+
+public interface Add {
+    public void initialize();
+    @FXML
+    public void addConfirm();
+}
---
+++ demo/src/main/java/com/example/demo/Edit.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import Util.FileUtilE;
+import javafx.fxml.FXML;
+
+public interface Edit {
+    public static void edit(Event selected){
+
+    }
+    public void addConfirm();
+}
