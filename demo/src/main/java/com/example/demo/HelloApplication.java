08fdeca7aa5cbf17e82dd1a0f3fe93dce8b36f29
wangyang12 <1075477190@qq.com>
2023-11-02 15:59:31 +1300

    Starting work on JavaDoc for login.

--- demo/src/main/java/com/example/demo/HelloApplication.java
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -9,12 +9,17 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 /**
- *
+ *The 'HelloApplication' class is the main entry point for the application, it displays the primary scene and offer methods to change to different scenes.
  */
 
 public class HelloApplication extends Application {
     private static Stage stage;
 
+    /**
+     * The entry point and initial stage for the application. It displays the login scene.
+     * @param stage The primary stage of the application.
+     * @throws IOException throws an error when initialize the scene.
+     */
     @Override
     public void start(Stage stage) throws IOException {
         HelloApplication.stage = stage;
@@ -27,6 +32,11 @@ public class HelloApplication extends Application {
         //stage.setScene(scene);
 
     }
+
+    /**
+     * Change the current scene to new scene by loading FXML file.
+     * @param fxml The FXMl file the stage is going to display.
+     */
     public static void changeStage(String fxml){
         Parent root = null;
         try {
@@ -35,9 +45,12 @@ public class HelloApplication extends Application {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
+    /**
+     * Loading FXML file, and open a new stage to add a member.
+     * @param fxml The FXML file
+     */
     public static void addM(String fxml){
         Stage stage = new Stage();
         Parent root = null;
